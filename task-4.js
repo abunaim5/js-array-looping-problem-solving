590aabd07980594277533812795e9176b235c988
abunaim5 <abunaim140@gmail.com>
2024-02-02 19:09:40 +0600

    another way without loop

--- task-4.js
+++ task-4.js
@@ -7,3 +7,8 @@ for (let word of strArr) {
     newStr = word + ' ' + newStr;
 }
 console.log(newStr);
+
+// Another way without loop
+const statement = 'I am a hard working person';
+let revStat = statement.split(' ').reverse().join(' ');
+console.log(revStat);
